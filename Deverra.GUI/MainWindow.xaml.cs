7e62bcffdce01f4bc9c0ed1c5a95ebf3d4191188
Dominik Andrzejczak <dersei0248@gmail.com>
2019-06-17 12:16:51 +0200

    New GUI style

--- Deverra.GUI/MainWindow.xaml.cs
+++ Deverra.GUI/MainWindow.xaml.cs
@@ -8,7 +8,7 @@ namespace Deverra.GUI
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
